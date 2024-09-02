c3a0d6905e3f4510af880f2d5407e97ce0d3aad3
altuseo <130089065+altuseo@users.noreply.github.com>
2024-09-03 01:22:31 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -422,7 +422,6 @@ def compare_keywords(keyword1, keyword2, api_key, search_engine, language, devic
         <p><a href="https://www.linkedin.com/in/altamash-mapari-44502a1a2/">Altamash Mapari</a> built this tool for SEOs so that everyone can enjoy and easily check the SERP Similarity in one click. The <strong>SERP Similarity Tool</strong> is a powerful, free SERP analysis tool designed to help you compare keyword SERP results and optimize your content strategy. This free SERP tool allows you to analyze live SERP data, understand keyword SERP overlap, and gain valuable insights into your SEO performance.</p>
  <h2>What is SERP Similarity?</h2>
         <p><strong>SERP Similarity</strong> refers to the comparison of search engine results pages (SERPs) for different keywords to identify commonalities and differences. By using this tool, you can analyze how similar or different the SERPs are for two keywords, helping you understand your competition and optimize your SEO strategies.</p>
-        
         <h2>How to Use the SERP Similarity Tool</h2>
         <ul>
             <li><strong>Get Your SerpAPI Key</strong>: To use this free SERP check tool, you'll need a SerpAPI key. Sign up for a free account on <a href="https://serpapi.com/">SerpAPI</a>. After registering, you can find your API key in the dashboard.</li>
@@ -444,7 +443,7 @@ def compare_keywords(keyword1, keyword2, api_key, search_engine, language, devic
         </ul>
         
         <p>This free SERP analysis tool is perfect for SEOs looking to gain quick insights into keyword competition and overlap. Start using this best free SERP tool today and gain valuable insights into your SEO strategy!</p>
-    
+    </div>
     """
 
     return similarity, table + additional_content
